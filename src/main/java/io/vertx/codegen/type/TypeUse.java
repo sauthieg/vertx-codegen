2df15ac2aab8c94469b254700c57dd2c7142d4f3
Julien Viet <julien@julienviet.com>
2016-10-15 19:13:15 +0200

    Experiments

--- src/main/java/io/vertx/codegen/type/TypeUse.java
+++ src/main/java/io/vertx/codegen/type/TypeUse.java
@@ -1,15 +1,28 @@
 package io.vertx.codegen.type;
 
+import com.sun.source.tree.AnnotatedTypeTree;
+import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ParameterizedTypeTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.util.Trees;
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.util.Context;
 import io.vertx.codegen.Helper;
 import io.vertx.codegen.annotations.Nullable;
 
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedParameterizedType;
 import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
@@ -90,6 +103,116 @@ public abstract class TypeUse {
           }
         }
         return false;
+      }
+    };
+  }
+
+  private static TypeUse NULL_TYPE_USE = new TypeUse() {
+    @Override
+    public TypeUse getArg(int index) {
+      return NULL_TYPE_USE;
+    }
+    @Override
+    public boolean isNullable() {
+      return false;
+    }
+  };
+
+  private interface InternalType {
+    boolean isNullable();
+    InternalType getArgAt(int index);
+  }
+
+  private static class ReflectType implements InternalType {
+    final Type type;
+    final boolean nullable;
+    private ReflectType(AnnotatedType annotated) {
+      this.type = annotated.getType();
+      this.nullable = TypeUse.isNullable(annotated);
+    }
+    public boolean isNullable() {
+      return nullable;
+    }
+    public InternalType getArgAt(int index) {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  private static class MirrorType implements InternalType {
+    final Tree type;
+    final boolean nullable;
+    private MirrorType(Tree type) {
+      this.type = type;
+      this.nullable = TypeUse.isNullable(type);
+    }
+    public boolean isNullable() {
+      return nullable;
+    }
+    public InternalType getArgAt(int index) {
+      ParameterizedTypeTree parameterizedType = (ParameterizedTypeTree) type;
+      return new MirrorType(parameterizedType.getTypeArguments().get(index));
+    }
+  }
+
+  public static TypeUse createReturnTypeUse(ProcessingEnvironment env, ExecutableElement... methods) {
+    Trees trees = Trees.instance(env);
+    InternalType[] abc = new InternalType[methods.length];
+    for (int i = 0; i < methods.length;i++) {
+      MethodTree tree = trees.getTree(methods[i]);
+      if (tree != null) {
+        abc[i] = new MirrorType(tree.getReturnType());
+      } else {
+        Method methodRef = Helper.getReflectMethod(methods[i]);
+        if (methodRef != null) {
+          AnnotatedType annotated = methodRef.getAnnotatedReturnType();
+          abc[i] = new ReflectType(annotated);
+        }
+      }
+    }
+    return createTypeUse(abc);
+  }
+
+  private static boolean isNullable(Tree type) {
+    TypeMirror typeMirror = ((JCTree)type).type;
+    if (typeMirror instanceof com.sun.tools.javac.code.Type.AnnotatedType) {
+      com.sun.tools.javac.code.Type.AnnotatedType abc = (com.sun.tools.javac.code.Type.AnnotatedType) typeMirror;
+      for (AnnotationMirror mirror : abc.getAnnotationMirrors()) {
+        if (mirror.getAnnotationType().toString().equals(NULLABLE)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  private static boolean isNullable(AnnotatedType type) {
+    for (Annotation annotation : type.getAnnotations()) {
+      if (annotation.annotationType().getName().equals(NULLABLE)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private static TypeUse createTypeUse(InternalType... types) {
+    return new TypeUse() {
+      @Override
+      public TypeUse getArg(int index) {
+        return TypeUse.createTypeUse(types[0].getArgAt(index));
+      }
+      @Override
+      public boolean isNullable() {
+        boolean nullable = false;
+        for (InternalType type : types) {
+          if (type.isNullable()) {
+            nullable = true;
+          } else {
+            if (nullable) {
+              throw new RuntimeException("Nullable type cannot override non nullable");
+            }
+          }
+        }
+        return nullable;
       }
     };
   }
